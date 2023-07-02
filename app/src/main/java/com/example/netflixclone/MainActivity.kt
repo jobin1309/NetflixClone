5e86c97cceabb498400395d9654a32eaf837bc19
Jobin mathew <jobin1309@gmail.com>
2023-07-02 13:12:37 +0530

    starting

--- app/src/main/java/com/example/netflixclone/MainActivity.kt
+++ app/src/main/java/com/example/netflixclone/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.netflixclone
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.netflixclone.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController;
+    private lateinit var binding: ActivityMainBinding;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHost
+
+        navController = navHostFragment.navController;
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
